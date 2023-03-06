5ae275a99fff487106e4d90d5bb5760a33b3bc11
alishachishtii <126850747+alishachishtii@users.noreply.github.com>
2023-03-06 09:01:29 +0500

    Add files via upload

---
+++ oop lab task 2.cpp
@@ -0,0 +1,61 @@
+/*create a class duration in c++ that has 4 data members days ,hours ,minutes and seconds.
+define a null constructor and 2 parametric constructors ,first constructor takes all the parameters
+and assigns to the data members .second constructor takes duration in seconds calculates days hours and minutes from
+that and sets the data members .also define 2 functions the first displays the duration in an understandable pattern
+.the second function shows the total duration in seconds .
+create different objects using the constructors and utilize the functions.*/
+#include<iostream>
+using namespace std;
+class Duration
+{
+	public:
+	//default constructor
+	int days , hours , minutes , seconds ;
+	Duration(){
+		days = 0;
+		hours = 0;
+		minutes = 0;
+		seconds = 0;
+	}
+	//constructor 1
+	Duration( int days, int hours , int minutes , int seconds ) {
+            this -> days = days;
+            this -> hours = hours;
+            this -> minutes = minutes;
+            this -> seconds = seconds;
+	}
+	// constructor 2
+	Duration( int seconds ){
+		    days = seconds / 86400;
+            seconds -= days * 86400;
+            hours = seconds / 3600;
+            seconds -= hours * 3600;
+            minutes = seconds / 60;
+            seconds -= minutes * 60;
+            seconds = seconds % 60;
+	}
+	
+	void display()
+	{
+		cout<<" | days :  "<< days <<"|" <<" | hours : "<< hours <<"|" <<" | minutes : "<< minutes <<"|" <<" | seconds : "<< seconds <<" |" <<endl;
+	}
+	int ConvertSeconds(){
+            return days * 86400 + hours * 3600 + minutes * 60 + seconds ;	
+	}
+};
+int main(){
+   Duration d1;
+   Duration d2(4,3,5,9);
+   Duration d3(4200);
+   
+   d1.display();
+    cout<<"Duration in seconds = "<<d1.ConvertSeconds()<<endl;
+
+    d2.display();
+    cout<<"Duration in seconds = "<<d2.ConvertSeconds()<<endl;
+
+    d3.display();
+    cout<<"Duration in seconds = "<<d3.ConvertSeconds()<<endl;
+   
+   return 0;
+}
